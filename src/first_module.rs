41540d0d76fe424ead274049573b9a6560d27eea
Moazzam Adil Khan <moaadi3890@gmail.com>
2020-06-21 00:13:40 +0500

    Comments added in modules

--- src/first_module.rs
+++ src/first_module.rs
@@ -1,8 +1,8 @@
 use actix_web::{web, HttpResponse};
 
-pub fn first_mod(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/1stmod")
+pub fn first_mod(cfg: &mut web::ServiceConfig) {// make a function in first module
+    cfg.service( //applied service method on cfg
+        web::resource("/1stmod")//use of resource at path /1stmod
             .route(web::get().to(||HttpResponse::Ok().body("1st Module")))
             .route(web::head().to(||HttpResponse::MethodNotAllowed())),
     );
--- src/second_module.rs
+++ src/second_module.rs
@@ -1,8 +1,8 @@
 use actix_web::{web, HttpResponse};
 
-pub fn api(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/2ndmod")
+pub fn api(cfg: &mut web::ServiceConfig) {// make a function in second module
+    cfg.service( //applied service method on cfg
+        web::resource("/2ndmod") //use of resource at path /api/2ndmod
             .route(web::get().to(||HttpResponse::Ok().body("2nd Module")))
             .route(web::head().to(||HttpResponse::MethodNotAllowed())),
     );
